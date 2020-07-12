8b747ff7941caae0a3a339a3aff8cf304748675b
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-12 22:41:44 +0800

    route issue fixed

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const postsController = require("../../controller/control.js");
+const postsController = require("../controller/control.js");
 
 router.route("/")
 router.get(postsController.findAll)
