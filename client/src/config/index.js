8594ddebd9e98aa450f2225f8af5d052a5dddccb
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-13 06:45:10 +0800

    fixed ap axious2

---
+++ client/src/config/index.js
@@ -0,0 +1,6 @@
+const dbuser = "heroku_07lhttrp";
+const dbcode = "42av2dqmnoqhqdrask0dvno704";
+
+const MONGODB_URI = `mongodb://${dbuser}:${dbcode}@ds263638.mlab.com:63638/heroku_07lhttrp`
+
+module.exports = MONGODB_URI;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import URI from "../config/index"
 
 const api = axios.create({
-  baseURL: process.env.MONGODB_URI || 'http://localhost:3000/'
+  baseURL: process.env.MONGODB_URI || URI
 })
 
 export const getPosts = () => api.get("api/assignment")
