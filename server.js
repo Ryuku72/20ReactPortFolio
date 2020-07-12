b0d3ca02aa564fc192493100ea5296df90ecf170
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-12 20:59:39 +0800

    added basic routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+//NPM packages
 const express = require("express");
-const path = require("path");
-const app = express();
+const mongoose = require("mongoose");
 
 //Port
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve up static assets (usually on heroku)
@@ -10,11 +11,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//Routes
+app.use(require("./routes/html.js"));
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
 
 
 // Start the API server
