d0dce7f32038ef67f9360902300067087dee82a3
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-12 18:11:03 +0800

    attempt 2

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
-//const mongoose = require("mongoose");
-const routes = require("./routes");
+const path = require("path");
 const app = express();
 const chalk = require("chalk")
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+    app.use(express.static("client/build"));
+  }
 
-// Connect to the Mongo DB
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+  // Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 // Start the API server
 app.listen(PORT, function () {
