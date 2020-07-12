bd8c4036e4a2bd40b00ee9bf92b2f52834481bf9
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-13 04:28:30 +0800

    fixed spelling mistake in utiliz

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api || "process.env.MONGODB_URI/api";',
+  baseURL: 'http://localhost:3000/' || process.env.MONGODB_URI,
 })
 
-export const getPosts = () => api.get("/projectinfo")
+export const getPosts = () => api.get("api/projectinfo")
 
 const apis = { getPosts }
 
