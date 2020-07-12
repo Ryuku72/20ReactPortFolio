26adf9ee1452efa7557ae53f42c40dd1ea0b00e6
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-13 06:40:39 +0800

    fixed ap axious

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/' || process.env.MONGODB_URI,
+  baseURL: process.env.MONGODB_URI || 'http://localhost:3000/'
 })
 
 export const getPosts = () => api.get("api/assignment")
