8aa3a464e8e8b2c4434dd0d5b639e31c0612c36d
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-12 19:22:37 +0800

    added axious

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const chalk = require("chalk")
+
+//Port
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+  app.use(express.static("client/build"));
+}
 
-  // Send every request to the React app
+// Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 // Start the API server
 app.listen(PORT, function () {
