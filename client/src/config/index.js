87aeee80a4cbcca4db6d1d9faa5669f697191c43
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-13 15:58:32 +0800

    working on access to db

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,10 +1,12 @@
 const dbuser = 
 //"heroku_07lhttrp" 
-process.env.DB_USER ;
+//process.env.DB_USER ;
+"ryuku72";
 
 const dbcode = 
 //"42av2dqmnoqhqdrask0dvno704";
-process.env.DB_PASS;
+//process.env.DB_PASS;
+"7201Ryuku";
 
 const MONGODB_URI = (`mongodb://${dbuser}:${dbcode}@ds263638.mlab.com:63638/heroku_07lhttrp`, {useNewUrlParser: true})
 
